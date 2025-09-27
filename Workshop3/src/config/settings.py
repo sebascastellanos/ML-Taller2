e9e6902d2ab46b8392e51b183cbb7f34cc821e74
sebascastellanos <sebastian.castellanos2012@gmail.com>
2025-09-27 10:42:46 -0500

    settings

--- Workshop3/src/config/settings.py
+++ Workshop3/src/config/settings.py
@@ -1,14 +1,22 @@
 """
 settings.py
-Configuración central del proyecto.
-Ajusta rutas y parámetros globales acá.
+<Configuración central del proyecto.
 """
 from pathlib import Path
 
-# .../Proyecto
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATA_DIR = PROJECT_ROOT / "data"
 PREGUNTAS_DIR = PROJECT_ROOT / "preguntas"
 
-# Semillas/constantes
+# === Dataset UCI Cleveland via URL ===
+TARGET_NAME = "target"        # nombre de la etiqueta
+DATA_PATH = None              # usamos URL, así que mantenlo en None
+DATA_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
+
+# columnas porque el CSV no trae header
+COLUMNS = [
+    'age','sex','cp','trestbps','chol','fbs','restecg','thalach',
+    'exang','oldpeak','slope','ca','thal','target'
+]
+
 RANDOM_STATE = 42
